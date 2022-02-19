30513a68aa63c77e08b6c21668653381a0b460a9
Jeremiasz Charewicz <jeremiasz.charewicz@microsoft.wsei.edu.pl>
2022-02-20 00:51:08 +0100

    Zmiana logowania / rejestrowania na polski

---
+++ Projekt .net/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Projekt_.net.Database;
+
+[assembly: HostingStartup(typeof(Projekt_.net.Areas.Identity.IdentityHostingStartup))]
+namespace Projekt_.net.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

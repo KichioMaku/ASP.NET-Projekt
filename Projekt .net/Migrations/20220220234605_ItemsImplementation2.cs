8c363ab4b145a8ea7e6236ccf25a5d3e597844e0
Mateusz Charewicz <mateusz.charewicz@microsoft.wsei.edu.pl>
2022-02-21 00:54:35 +0100

    Repair

---
+++ Projekt .net/Migrations/20220220234605_ItemsImplementation2.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Projekt_.net.Migrations
+{
+    public partial class ItemsImplementation2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "Orders",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "Items",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OwnerId",
+                table: "Orders",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_OwnerId",
+                table: "Items",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_AspNetUsers_OwnerId",
+                table: "Items",
+                column: "OwnerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AspNetUsers_OwnerId",
+                table: "Orders",
+                column: "OwnerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_AspNetUsers_OwnerId",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AspNetUsers_OwnerId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_OwnerId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Items_OwnerId",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Items");
+        }
+    }
+}
